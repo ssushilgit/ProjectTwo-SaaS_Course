0e66ad046935689ad74cc9fd21b33d68f6d9dee1
ssushilgit <shresthasushil741@gmail.com>
2025-06-02 01:23:45 +0545

    Table column creation

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,4 +1,4 @@
-import {Sequelize} from 'sequelize'
+import {Sequelize} from 'sequelize-typescript'
 
 const sequelize = new Sequelize({
     database : process.env.DB_NAME , // database ko name 
@@ -6,7 +6,9 @@ const sequelize = new Sequelize({
     password :process.env.DB_PASSWORD,  // database ko password, by default ""
     host : process.env.DB_HOST, // database ko location, kaha xa vanne kura, localhost(mycomputer)
     dialect : "mysql", // k database use garna aateko vanne kura, 
-    port : Number(process.env.DB_PORT)
+    port : Number(process.env.DB_PORT),
+    models : [__dirname + '/models'] // current location + '/models'
+
 })
 
 
@@ -17,6 +19,13 @@ sequelize.authenticate()
 .catch((error)=>{
     console.log(error)
 })
+
+// migrate garnu parxa/ push garnu parxa 
+sequelize.sync({force:true})
+.then(()=>{
+    console.log("Migrated Successfully. New changes!!!")
+})
+
 
 
 export default sequelize
---
+++ server/src/database/models/user.model.ts
@@ -0,0 +1,39 @@
+import { Table, Column, Model, DataType, PrimaryKey } from "sequelize-typescript";
+
+@Table({
+    tableName : 'users',
+    modelName : 'User',
+    timestamps : true
+})
+
+
+class User extends Model{
+    @Column({
+        primaryKey : true, 
+        type : DataType.UUID, 
+        defaultValue : DataType.UUIDV4
+    })
+    declare id : string
+    
+    @Column({
+        type : DataType.STRING
+    })
+    declare username : string 
+
+    @Column({
+        type : DataType.STRING
+    })
+    declare password : string 
+
+    @Column({
+        type : DataType.STRING
+    })
+    declare email:string
+    @Column({
+        type : DataType.ENUM('teacher','institute','super-admin','student'), 
+        defaultValue : 'student', 
+    })
+    declare role:string
+}
+
+export default User 
