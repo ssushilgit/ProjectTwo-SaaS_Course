a9aa85a3940d75485e69b5c645895eb5775c2112
ssushilgit <shresthasushil741@gmail.com>
2025-07-13 01:16:43 +0545

    cloudinary setup

---
+++ server/src/services/cloudinaryConfig.ts
@@ -0,0 +1,9 @@
+import {v2 as cloudinary} from 'cloudinary'
+import {CloudinaryStorage} from 'multer-storage-cloudinary'
+
+
+ cloudinary.config({ 
+        cloud_name: 'dyxsj9vrs', 
+        api_key: '874748557886589', 
+        api_secret: 'n6AEY22Uj6ptRVBqrzqU8Q_jPuM' // Click 'View API Keys' above to copy your API secret
+})
